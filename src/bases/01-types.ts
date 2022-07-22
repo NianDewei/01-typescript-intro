0d051139a5d82b9b2992531a70d165f67e8ce921
Rolando Oc <admi.jmb@gmail.com>
2022-07-22 17:16:22 -0500

    02 | More about data types

--- src/bases/01-types.ts
+++ src/bases/01-types.ts
@@ -1,3 +1,9 @@
-export const name = "Rolando"
+export let name = "Rolando"
 export const age: number = 28
 export const isValid: boolean = true
+
+name = "Gilver"
+// name=123
+// name=true
+
+export const templateString = `It's a template String`
